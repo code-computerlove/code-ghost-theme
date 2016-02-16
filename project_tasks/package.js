0df6adf34b0f31871c6f06bbd3d42a363ef631d7
Darren Hall <itsdarrenhall@gmail.com>
2016-02-16 11:26:27 +0000

    Fix bug in variable scope Add warning when package task is ran without --prod flag

--- project_tasks/package.js
+++ project_tasks/package.js
@@ -6,15 +6,20 @@
 \* ============================================================ */
 
 var gif				= require('gulp-if');
+var gutil 			= require('gulp-util');
 
 var zip				= require('gulp-zip');
 var preprocess		= require('gulp-preprocess');
 
 module.exports = function(gulp, config, tasks) {
 
-	config.isProd = true;
-
 	gulp.task('package', tasks.default, function() {
+
+		if(!config.isProd) {
+			gutil.log(gutil.colors.red('Packaging development assets'));
+			gutil.log(gutil.colors.red('Use --prod flag to package production assets'));
+		}
+
 	    return gulp.src(config.paths.dest.html + '/**/*')
 	        .pipe(zip(config.files.package))
 	        .pipe(gulp.dest(config.dirs.root));
