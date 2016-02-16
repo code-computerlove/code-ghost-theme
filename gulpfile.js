8fdedc9239e274a181e261c70630158777278f44
Darren Hall <itsdarrenhall@gmail.com>
2016-02-16 08:57:13 +0000

    Remove console.log

--- gulpfile.js
+++ gulpfile.js
@@ -46,5 +46,3 @@ require('./project_tasks/scripts.js')(gulp, config, tasks);
 
 gulp.task('watch', tasks.watch);
 gulp.task('default', tasks.default.concat(tasks.watch));
-
-console.log(config.isProd, tasks);
